4c8b9c141fc5690ec5d64d67841807314c3ee94a
tchikou-ramy <ramytch10@gmail.com>
2022-12-06 13:19:56 +0100

    just SEND it

---
+++ contactList.js
@@ -0,0 +1,17 @@
+const contactList = [
+  {
+    lastName: 'Ben Lahmer',
+    firstName: 'Fares',
+    email: 'fares@gmail.com',
+    age: 26,
+  },
+  { lastName: 'Kefi', firstName: 'Seif', email: 'kefi@gmail.com', age: 15 },
+  {
+    lastName: 'Fatnassi',
+    firstName: 'Sarra',
+    email: 'sarra.f@gmail.com',
+    age: 40,
+  },
+  { lastName: 'Ben Yahia', firstName: 'Rym', age: 4 },
+  { lastName: 'Cherif', firstName: 'Sami', age: 3 },
+];
